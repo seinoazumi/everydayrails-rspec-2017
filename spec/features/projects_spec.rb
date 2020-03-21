7963e1e252fafb8696aa0356e8754c3414105b74
okutamatanuki <seinoazumi@gmail.com>
2020-03-21 19:43:09 +0900

    テストをメソッドにまとめ可読性を上げる

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -7,23 +7,30 @@ RSpec.feature "Projects", type: :feature do
   scenario "ユーザーは新しいプロジェクトを作成する" do
 
     sign_in user
-    visit root_path
+    go_to_project "New Project"
 
-    expect(page).to have_content "New Project"
     expect {
-      click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
-      click_button "Create Project"
+      create_project
     }.to change(user.projects, :count).by(1)
 
-    aggregate_failurs do
+    aggregate_failures do
       expect(page).to have_content "Project was successfully created"
       expect(page).to have_content "Test Project"
       expect(page).to have_content "Owner: #{user.name}"
     end
   end
 
+  def go_to_project(name)
+    visit root_path
+    click_link name
+  end
+
+  def create_project
+    fill_in "Name", with: "Test Project"
+    fill_in "Description", with: "Trying out Capybara"
+    click_button "Create Project"
+  end
+  
   # 失敗をテストするために書かれたテスト
   # scenario "ゲストがプロジェクトを追加する" do
   #   visit projects_path
